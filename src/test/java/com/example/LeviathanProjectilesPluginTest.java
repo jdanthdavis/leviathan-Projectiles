02402dd7fef42ab3e744031a463ff9bd093cb198
jdanthdavis <87504521+jdanthdavis@users.noreply.github.com>
2024-11-18 00:53:19 -0500

    Adds LeviathanProjectiles

--- src/test/java/com/example/LeviathanProjectilesPluginTest.java
+++ src/test/java/com/example/LeviathanProjectilesPluginTest.java
@@ -3,11 +3,11 @@ package com.example;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class LeviathanProjectilesPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(LeviathanProjectilesPlugin.class);
 		RuneLite.main(args);
 	}
 }
